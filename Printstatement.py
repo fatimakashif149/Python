44962fe9d831998d3b25b9bdf022acd610482f64
fatimakashif149 <fatimakashif1235@gmail.com>
2026-01-16 22:35:35 +0500

    print statement again

---
+++ Printstatement.py
@@ -0,0 +1,7 @@
+#Printing a greeting 
+print("Helllo World")
+#Printing my name
+print("My Name Is Fatima")
+#Printing my hobbies
+print("My Hobbies include reading,taekwondo,sketching,writing")
+print("The ressult of 3+5 is=",3+5)
