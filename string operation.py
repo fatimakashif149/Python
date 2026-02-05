a0a1bd61edcfb281e877701203317ae9bc4f0a7b
fatimakashif149 <fatimakashif1235@gmail.com>
2026-02-05 21:51:12 +0500

    hello how are you

---
+++ string operation.py
@@ -0,0 +1,4 @@
+text = str(input("Enter a string:"))
+revText=text[::-1]
+print("Reverse given string is:")
+print(revText)
---
+++ typecasting.py
@@ -0,0 +1,13 @@
+a = "helo"
+age = 10
+height = 168.8
+student = False
+print("type of a:",type(a))
+print("type of age:",type(age))
+print("type of height:",type(height))
+print("type of student:",type(student))
+print("after typecasting")
+age = str(age)
+print("type of age:",type(age))
+height = int(height)
+print("type of height:",type(height))
