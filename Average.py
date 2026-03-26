53a6de5558d76356207e89cbec79cd0d860d9831
fatimakashif149 <fatimakashif1235@gmail.com>
2026-03-26 21:25:59 +0500

    hello ig

---
+++ Average.py
@@ -0,0 +1,10 @@
+tree1 = 95
+tree2 = 98
+tree3 = 89
+tree4 = 99
+tree5 = 100
+
+sum = (tree1+tree2+tree3+tree4+tree5)
+print("The sum of all the trees is :", sum)
+Average = sum/5 
+print("The average length of trees is:", Average)
